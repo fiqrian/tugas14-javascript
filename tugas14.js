6ee0463f783770639e8b7ba11d7ef37032000474
Fiqrian <fiqrian.faturahman@gmail.com>
2021-12-02 00:09:17 +0700

    tugas 14 javascript

---
+++ tugas14.js
@@ -0,0 +1,9 @@
+const UlangTahun = () => {
+  var ultah = 37 / 9;
+  console.log(ultah);
+  console.log(Math.ceil(ultah));
+  console.log(Math.floor(ultah));
+  console.log(Math.round(ultah));
+};
+
+console.log(UlangTahun());
